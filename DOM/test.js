be634731be3fe222f06293b9f6967315b1335cca
DESKTOP-8M0J74Q\User <nwy140@gmail.com>
2018-05-08 18:46:21 +0800

    DOM initialized

---
+++ DOM/test.js
@@ -0,0 +1,59 @@
+  // getElementById
+function getElementById(){
+  var elementText = document.getElementById("getElementByIdExample");
+  alert(elementText.innerTest);
+}
+
+  // getElementById
+
+
+  //getElementByTagName
+
+
+  //createElement
+
+
+  //getElementStyle
+
+
+  //changeElementStyle
+
+
+  //element.style.property
+
+
+  //appendChild
+
+
+  //removeChild
+
+
+  // This is a JavaScript object
+  var fruits = {
+    0:{
+      "name": "Apple",
+      "price": 50,
+      "weight": 125
+    },
+    1:{
+      "name": "Orange",
+      "price": 60,
+      "weight": 230
+    },
+    2:{
+      "name": "Pear",
+      "price": 35,
+      "weight": 200
+    },
+    3:{
+      "name": "Durian",
+      "price": 120,
+      "weight": 500
+    }
+  }
+
+
+  //print loop
+
+
+  //clearing list
